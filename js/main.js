570bae666003af440979223257225257dbd2dd41
“Boris <bmedved78@mail.ru>
2023-03-09 18:18:02 +0300

    add_const

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+  const cmsMainTitle = document.querySelector(".cms__title"),
+    modalForm = document.querySelector(".modal__form"),
+    modalCheckbox = document.querySelector(".modal__checkbox"),
+    modalInputDiscount = document.querySelector(".modal__input_discount");
+})();
